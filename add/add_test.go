e5d2d61d680dc568d0e000e4a515581f770e11ab
dkaiser014 <dkaiser014@gmail.com>
2021-01-25 14:27:55 +0100

    Tests: written the tests for the add command

--- add/add_test.go
+++ add/add_test.go
@@ -1,17 +1,66 @@
 package main
 
-/*import (
+import (
 	"os"
-	"strings"
 	"testing"
-	"runtime"
-	"path/filepath"
+	"io/ioutil"
 	"encoding/json"
-)*/
+)
 
-/*
-*
-* > TODO's
-* - Write the tests for the add.go command
-*
-*/
+// TempFiles represents a structure for the dummy temp_files.json
+type TestTempFiles struct {
+	Filepaths []string
+	Created string
+}
+
+// Dummy temp_files.json
+var tempFilesJSON = `
+	{
+		"Filepaths": [
+			"/home/user/Documents/example_dir",
+			"/home/user/Documents/example_file.txt",
+			"/home/user/Documents/example_subfolder",
+			"/home/user/Documents/example_subfolder/example_subfile.txt",
+		],
+		"Created": "2021-01-25 13:49:46"
+	}
+`
+
+// Test to check if there are files
+// inside the directory inputed by the user
+func TestIsDirEmpty(t *testing.T) {
+	// Getting the working directory
+	rootDir, err := os.Getwd()
+	if err != nil {
+		expected := true
+		got := false
+	
+		if got != expected {
+			t.Errorf("Expected: %v, got: %v", expected, got)
+		}
+	}
+
+	// Scaning the directory
+	files, err := ioutil.ReadDir(rootDir)
+	
+	if err != nil {
+		expected := true
+		got := false
+
+		if got != expected {
+			t.Errorf("Expected: %v, got: %v, files: %v", expected, got, files)
+		}		
+	}
+}
+
+// Test to check the dummy temp_files.json file
+func TestTempFilesJSON(t *testing.T) {
+	var testTempFiles TestTempFiles
+	json.Unmarshal([]byte(tempFilesJSON), &testTempFiles)
+
+	expected := testTempFiles.Filepaths
+	got := testTempFiles.Filepaths
+	if got != nil {
+		t.Errorf("Expected: %v, got: %v", expected, got)
+	}
+}
