88462772ea9ae2fd8ba904d826046767ddf90079
dkaiser014 <dkaiser014@gmail.com>
2021-01-30 15:52:18 +0100

    Working: added todolist to the push command file

---
+++ push/push.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "log"
+  "path/filepath"
+  "io/ioutil"
+)
+
+/*
+*
+* TODO
+* > Take console argument as input and check them
+* > Take all the files inside the folder inputed by the user
+* > Copy them to the backup directory
+* > Display a success message
+*
+*/
+
+func main() {
+	fmt.Println(`
+________ _____  __________ 
+___  __ )__  / / /___  __ \
+__  __  |_  / / / __  /_/ /
+_  /_/ / / /_/ /  _  ____/ 
+/_____/  \____/   /_/     
+	`)
+}
